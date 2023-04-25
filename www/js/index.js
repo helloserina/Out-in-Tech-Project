badb45875b7dfecafbf2fc450feb61d6d14bf575
Serina Khanna <skhanna@ucsd.edu>
2023-04-25 18:13:29 +0200

    made changes to homepage

--- www/js/index.js
+++ www/js/index.js
@@ -19,6 +19,9 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
+let numberOfKeywords = 10
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -26,4 +29,35 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    insertKeywordCards()
+}
+
+function insertKeywordCards() {
+    document.getElementById('column-start').appendChild(createKeywordCard())
+}
+
+function createKeywordCard() {
+    const divWrapper = document.createElement('div').classList.add('card text-center align-items-center');
+    divWrapper.style.width = '70%'
+    const img = document.createElement('img').src = "../www/img/boxed_water_unsplash.jpg"
+    img.classList.add("card-img-top")
+    const cardBody = document.createElement('div').classList.add("card-body text-center")
+    const title = document.createElement('h5').classList.add('card-title')
+    title.innerHTML = "Alzheimer's Disease" // insert given text
+    const description = document.createElement('p').classList.add('card-text')
+    // insert given description
+    description.innerHTML = "Alzheimer's disease is a brain disorder that slowly destroys memory and thinking skills and, eventually, the ability to carry out the simplest tasks"
+    const button = document.createElement('a').classList.add('btn btn-primary')
+    // button links to nothing at the moment
+    button.innerHTML = 'Read more'
+    // must add an id to the divWrapper to identify a particular keyword/subject
+
+    cardBody.appendChild(title)
+    cardBody.appendChild(description)
+    cardBody.appendChild(button)
+
+    divWrapper.appendChild(img)
+    divWrapper.appendChild(cardBody)
+
+    return divWrapper
 }
